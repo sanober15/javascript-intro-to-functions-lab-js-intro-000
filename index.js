7bd82998376b5f99f12be8278d56c473d11da744
sanober riaz <sanober@sanober.me>
2017-12-27 06:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function shout(input){
   return input.toUpperCase();
 }
+
+function whisper(string){
+  return string.toLowerCase();
+
+}
+
+function logShout(string){
+  var out = string.toUpperCase();
+  console.log(`${out}`);
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase);
+}
